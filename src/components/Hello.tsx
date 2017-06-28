8c0bde2176beefa20d6cff8632122c42bc2dc1f5
Mitchell <mover333@gmail.com>
2017-06-27 17:40:07 -0700

    Added redux

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { connect } from 'react-redux';
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -6,6 +7,7 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the 'undefined' type.
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
+        connect();
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
