30da0e2738085b5353f7766c765ed85c5bd4bc95
Mitchell <mover333@gmail.com>
2017-06-27 17:44:32 -0700

    Added more scalfolding for redux

--- src/containers/Hello.tsx
+++ src/containers/Hello.tsx
@@ -7,7 +7,7 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the 'undefined' type.
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
-        connect();
+        
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
